ffb6d654842cbb7124669f2617f582e16fce43a2
Cristian <crisgues8@gmail.com>
2025-03-26 17:16:39 -0400

    Rename ScreenContext to final name with correct casing

---
+++ client/src/contexts/ScreenContext.js
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from "react";
+
+export const ScreenContext = createContext();
+
+export function ScreenProvider({ children }) {
+  const [currentScreen, setCurrentScreen] = useState("Search");
+
+  return (
+    <Screen.Provider value={{ currentScreen, setCurrentScreen }}>
+      {children}
+    </Screen.Provider>
+    //
+  );
+}
