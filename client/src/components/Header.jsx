f460bd29a528ca208814eab2ac1d83d9f3dc9755
Cristian <crisgues8@gmail.com>
2025-03-28 18:47:17 -0400

    Made log out button functional

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaSignOutAlt } from "react-icons/fa";
+import { ScreenContext } from "../contexts/ScreenContext";
 import '../css/header.css'
 
 function Header() {
+
+  const { setCurrentScreen } = useContext(ScreenContext)
+
+  function logOut() {
+    setCurrentScreen("Login");
+  }
+
   return (
     <header>
       <h1>StudyCircle</h1>
-      <button className="logout-btn" title="Cerrar sesión">
+      <button className="logout-btn" title="Cerrar sesión" onClick={logOut}>
         <FaSignOutAlt />
       </button>
     </header>
