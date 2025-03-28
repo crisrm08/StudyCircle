49ef76e0d8fd140ab22ed13cb52acebd9e00a4c5
Cristian <crisgues8@gmail.com>
2025-03-28 18:39:17 -0400

    Added log out button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { FaSignOutAlt } from "react-icons/fa";
 import '../css/header.css'
 
 function Header() {
   return (
     <header>
       <h1>StudyCircle</h1>
+      <button className="logout-btn" title="Cerrar sesión">
+        <FaSignOutAlt />
+      </button>
     </header>
   );
 }
