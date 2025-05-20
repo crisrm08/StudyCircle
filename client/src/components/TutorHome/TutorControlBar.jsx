b64309c9fd46340914defcb42f419b0782ee438a
Cristian <crisgues8@gmail.com>
2025-05-20 10:56:09 -0400

    tutor top bar done

---
+++ client/src/components/TutorHome/TutorControlBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../css/tutorHomeStyles/tutorcontrolbar.css"
+
+function TutorControlBar() {
+    return(
+        <div className="tutor-control-bar">
+            <h1>Historial</h1>
+            <span>|</span>
+            <h1>Solicitudes</h1>
+        </div>
+    )
+}
+
+export default TutorControlBar;
--- client/src/components/TutorHome/TutorHomeScreen.jsx
+++ client/src/components/TutorHome/TutorHomeScreen.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import Header from "../Common/Header";
+import TutorTopBar from "./TutorTopBar";
+import TutorControlBar from "./TutorControlBar";
 import RequestBox from "../TutorHome/RequestBox";
 import StudentSidebar from "../Common/StudentSidebar";
 import { SidebarContext } from "../../contexts/SidebarContext";
@@ -11,6 +13,16 @@ function TutorHomeScreen() {
     return(
         <div>
             <Header/>
+            <TutorTopBar 
+                name="Carlos Santana"
+                avatar="https://randomuser.me/api/portraits/men/32.jpg"
+                rating={4}
+                subjects={["Derivadas", "Series y secuencias", "Límites", "Ecuaciones", "Integrales"]}
+                price={800}
+                schedule={["Lunes 16:00 - 18:00", "Miércoles 19:00 - 22:00"]}
+            />
+            <TutorControlBar />
+
             <RequestBox avatar={"https://randomuser.me/api/portraits/men/12.jpg"} />
             <RequestBox avatar={"https://randomuser.me/api/portraits/men/12.jpg"} />
             <RequestBox avatar={"https://randomuser.me/api/portraits/men/12.jpg"} />
