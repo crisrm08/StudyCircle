eaf3f06b4af3afb40875b7f7aee217c4077e904e
Cristian <crisgues8@gmail.com>
2025-05-28 16:17:19 -0400

    db connection

--- server/index.js
+++ server/index.js
@@ -9,5 +9,14 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+db.connect();
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
