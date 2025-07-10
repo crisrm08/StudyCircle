4fa927f158a9e425b299dcc5bca9b48be2f34699
Cristian <crisgues8@gmail.com>
2025-07-10 17:59:30 -0400

    Avoid tutors from entering search screen

--- client/src/components/Search/SearchScreen.jsx
+++ client/src/components/Search/SearchScreen.jsx
@@ -1,4 +1,6 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../../contexts/UserContext";
 import Header from "../Common/Header";
 import SubjectSelection from "./SubjectSelection";
 import StudentSidebar from "../Common/StudentSidebar";
@@ -9,6 +11,16 @@ import '../../css/searchStyles/searchscreen.css';
 
 function SearchScreen() {
     const { isSidebarClicked, setIsSidebarClicked } = useContext(SidebarContext);
+    const navigate = useNavigate();
+    const { user , loading} = useUser();
+
+    useEffect(() => {
+        if (!loading && user?.profile_type === "tutor") {
+        navigate("/tutor-home-page", { replace: true });
+        }
+    }, [user, loading, navigate]);
+
+    if (loading) return null; 
     
     return(
         <div>
