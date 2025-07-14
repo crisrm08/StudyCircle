5e2b55f2f645764817b8cf11f2f160181416e10a
Cristian <crisgues8@gmail.com>
2025-07-14 11:37:57 -0400

    Added loading screen to results screen

--- client/src/components/Results/ResultScreen.jsx
+++ client/src/components/Results/ResultScreen.jsx
@@ -5,6 +5,7 @@ import { TimeContext } from "../../contexts/TimeContext";
 import Header from "../Common/Header";
 import StudentSidebar from "../Common/StudentSidebar";
 import TutorProfileCard from "./TutorProfileCard";
+import LoadingScreen from "../Common/LoadingScreen";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../../css/resultsStyle/resultscreen.css";
@@ -15,25 +16,27 @@ function ResultScreen() {
     const { topic, setTopic } = useContext(SubjectTopicContext);
     const { day, setDay, hour, setHour } = useContext(TimeContext);
     const [tutors, setTutors] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        console.log("selected topic: " + topic);
-        console.log("selected day: " + day);
-        console.log("selected hour: " + hour);
-        
         const params = {};
         if (topic) params.topic = topic;
         if (day)   params.day   = day;
         if (hour)  params.hour  = hour.toString();
 
+        setLoading(true);
         axios.get(`${process.env.REACT_APP_BACKEND_URL}/tutors`, { params })
-        .then(({ data }) => {
-            setTutors(data.tutors);
-            console.log(data.tutors);
-        })
-        .catch(console.error);
+            .then(({ data }) => {
+                setTutors(data.tutors);
+                setLoading(false);
+                console.log(data.tutors);
+            })
+            .catch(error => {
+                console.error(error);
+                setLoading(false);
+            });
     }, [topic, day, hour]);
-    
+
     useEffect(() => {
         const handlePopState = (event) => { navigate("/", { replace: true });};
 
@@ -43,29 +46,35 @@ function ResultScreen() {
             window.removeEventListener("popstate", handlePopState);
         };
     }, [navigate]);
-      
+
     return(
         <div>
-            <Header/>
-            <div className={`result-screen ${tutors.length === 0 ? "no-results" : ""}`}>
-                {tutors.length === 0
-                    ? <h1>No se encontraron tutores que coincidan con tu búsqueda</h1>
-                    : tutors.map(t => (
-                        <TutorProfileCard
-                        key={t.id}
-                        id={t.id}
-                        image={t.image}
-                        name={t.name}
-                        occupation={t.occupation}
-                        description={t.description}
-                        pricePerHour={t.pricePerHour}
-                        rating={t.rating}
-                        specialties={t.specialties}
-                        onExplore={() => navigate(`/tutor-facts/${t.id}`)}
-                        />
-                    ))
-                }
-            </div>
+            {loading ? (
+                <LoadingScreen />
+            ) : (
+                <div>
+                    <Header/>
+                    <div className={`result-screen ${tutors.length === 0 ? "no-results" : ""}`}>
+                        {tutors.length === 0
+                            ? <h1>No se encontraron tutores que coincidan con tu búsqueda</h1>
+                            : tutors.map(t => (
+                                <TutorProfileCard
+                                    key={t.id}
+                                    id={t.id}
+                                    image={t.image}
+                                    name={t.name}
+                                    occupation={t.occupation}
+                                    description={t.description}
+                                    pricePerHour={t.pricePerHour}
+                                    rating={t.rating}
+                                    specialties={t.specialties}
+                                    onExplore={() => navigate(`/tutor-facts/${t.id}`)}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
+            )}
             {isSidebarClicked && (
                 <>
                     <div className="overlay" onClick={() => setIsSidebarClicked(false)}/>
